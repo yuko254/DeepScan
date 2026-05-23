1da81edfe03af8dc2d4772739a2d1246e4d34593
Yuko <omaralmallah960@gmail.com>
2026-05-23 23:26:11 +0300

    tiny chore

--- src/server.ts
+++ src/server.ts
@@ -4,5 +4,5 @@ import * as env from "./config/env.js"
 
 const PORT = env.APP_PORT;
 app.listen(PORT, '0.0.0.0', () => 
-    console.log(`Server running on http://0.0.0.0:${PORT}`)
+    console.log(`✅ Server running on http://0.0.0.0:${PORT}`)
 );
